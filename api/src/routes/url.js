d35ae8dc5088ecedd6e439f42ad37af81dff3e2d
Samuel Ogu <hello@samuelogu.com>
2023-01-10 03:17:31 +0100

    add validator to route

--- api/src/routes/url.js
+++ api/src/routes/url.js
@@ -4,8 +4,8 @@ const url = require('../controllers/url.controller')
 const validator = require('../middlewares/validator')
 const schemas = require('../validators/url.validator')
 
-routes.get('/', () => {
-
-})
+routes.get('/:shortcode', url.shortcode)
+routes.get('/:shortcode/stat', url.stat)
+routes.post('/submit', validator(schemas.submit), url.submit)
 
 module.exports = routes;
